59f0bb91eac4d1f70088ce5752d2bb91ec0833d8
ladicle <aigarash@zlab.co.jp>
2017-11-06 08:43:12 +0900

    add README & boot script for osx

---
+++ osx/README.md
@@ -0,0 +1,22 @@
+# Setup OSX
+
+## Manual Playbooks
+
+1. Download apps from AppStore
+ 1. OnePassword
+ 2. Slack
+ 3. Evernote
+ 4. BetterSnapTool
+2. Setup OnePassword
+3. Open General Preference and Turn on iCloud(drive, photos and safari)
+3. Download following apps from Safari
+ 1. [Karabiner](https://pqrs.org/osx/karabiner/)
+ 2. [VSCode](https://code.visualstudio.com)
+ 3. [clipy](https://clipy-app.com)
+ 4. [iTerm2](https://www.iterm2.com)
+ 5. [Marxico](https://www.dropbox.com/sh/y6ylaff9r2fdx42/AAAgvPk5io9KuXRgsb_HooEea?dl=0)
+5. Set id_rsa keys
+4. Run the following command on Terminal
+```
+$ ./boot.sh
+```
---
+++ osx/boot.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cat <<EOF
+
+███████╗███████╗████████╗██╗   ██╗██████╗      ██████╗ ███████╗██╗  ██╗       ██╗
+██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗    ██╔═══██╗██╔════╝╚██╗██╔╝    ██╗╚██╗
+███████╗█████╗     ██║   ██║   ██║██████╔╝    ██║   ██║███████╗ ╚███╔╝     ╚═╝ ██║
+╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝     ██║   ██║╚════██║ ██╔██╗     ██╗ ██║
+███████║███████╗   ██║   ╚██████╔╝██║         ╚██████╔╝███████║██╔╝ ██╗    ╚═╝██╔╝
+╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝          ╚═════╝ ╚══════╝╚═╝  ╚═╝       ╚═╝
+
+EOF
+
+# check git command
+git version
+
+# show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles -boolean true
+killall Finder
+
+# install commands using brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap sachaos/todoist
+brew install \
+     aspell \
+     bazel \
+     imagemagick \
+     cheat \
+     cmigemo \
+     ctags \
+     emacs \
+     fish \
+     fzf \
+     gdb \
+     ag \
+     tig \
+     tree \
+     tmux \
+     go \
+     hub \
+     jq \
+     todoist \
+     dep
+
+# setup dotfiles
+[ ! -d $HOME/Developments ] && mkdir $HOME/Developments
+[ ! -d $HOME/Developments/src/github.com/Ladicle ] &&\
+    mkdir -p $HOME/Developments/src/github.com/Ladicle
+
+if [ ! -d $HOME/Developments/src/github.com/Ladicle/dotfiles ]; then
+    cd $HOME/Developments/src/github.com/Ladicle
+    git clone git@github.com:Ladicle/dotfiles.git
+fi
+
+cd $HOME/Developments/src/github.com/Ladicle/dotfiles
+bash install.sh osx
+
+# setup hub command
+cd $HOME
+hub issue || :
+
+# install go commands
+export GO_SRC=$HOME/Developments/src
+go get -u github.com/Ladicle/toggl
+go get -u golang.org/x/tools/...
